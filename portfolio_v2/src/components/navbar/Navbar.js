9827ca028ac63984c17e4fb723c7249e90a66615
Joey <j.p.huang@student.tudelft.nl>
2019-05-13 00:31:49 +0200

    Navbar completed

--- portfolio_v2/src/components/navbar/Navbar.js
+++ portfolio_v2/src/components/navbar/Navbar.js
@@ -7,11 +7,11 @@ import ironman from "../../images/ironman_logo.png";
 
 const Navbar = props => {
   return (
-    <nav className="navbar navbar-expand-lg mb-3 py-0">
+    <nav className="navbar navbar-expand-lg mb-0 py-0">
       <div className="container">
         <div className="col-lg-4">
           <a href="/" className="navbar-brand">
-            <img src={ironman} width="80" height="80" alt="" />
+            <img src={ironman} width="70" height="70" alt="" />
           </a>
         </div>
         <div className="col-lg-8">
@@ -26,10 +26,27 @@ const Navbar = props => {
                 Projects
               </a>
             </li>
-            <li className="nav-item mx-auto">
-              <a href="#">
-                <i className="fa fa-instagram" />
-              </a>
+            <li id="social" className="nav-item mx-auto">
+              <div className="mail">
+                <a href="#">
+                  <i className="fa fa-envelope-o fa-2x" />
+                </a>
+              </div>
+              <div className="instagram">
+                <a href="#">
+                  <i className="fa fa-instagram fa-2x" />
+                </a>
+              </div>
+              <div className="github">
+                <a href="#">
+                  <i className="fa fa-github fa-2x" />
+                </a>
+              </div>
+              <div className="linkedin">
+                <a href="#">
+                  <i className="fa fa-linkedin-square fa-2x	" />
+                </a>
+              </div>
             </li>
           </ul>
         </div>
